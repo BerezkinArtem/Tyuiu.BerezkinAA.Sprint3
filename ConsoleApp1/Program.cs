a1c7b253e3a5313903c664967dda9309bd0a2a76
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-10-20 04:20:51 +0500

    Task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+            int c = 3;
+            int d = 5;
+            int i = 1;
+            while (i < 4)
+            {
+                b++;
+                b -= a;
+                c = b - a;
+                d = (c + a) + i;
+                i ++;
+            }
+            Console.WriteLine(d);
+        }
+    }
+}
---
+++ Tyuiu.BerezkinAA.Sprint3.Task1.V12.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.BerezkinAA.Sprint3.Task1.V12.Lib
+{
+    public class DataService : ISprint3Task1V12
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+           double sumSeries = 1;
+            while (startValue <= stopValue) 
+            {
+                sumSeries = sumSeries + Math.Pow((1 / Math.Pow(startValue, value)), 2);
+                startValue += 1;
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
