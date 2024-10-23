944c36ec3c746fca85e2317244431c164d47b4a6
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-10-23 23:57:20 +0500

    Task1

---
+++ Tyuiu.BerezkinAA.Sprint3.Task1.V12.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+
+namespace Tyuiu.BerezkinAA.Sprint3.Task1.V12.Lib
+{
+    public class DataService : ISprint3Task1V12
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+            while (startValue <= stopValue)
+            {
+                sumSeries = sumSeries + Math.Pow((1 / Math.Pow(startValue, value)), 2);
+                startValue += 1;
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.BerezkinAA.Sprint3.Task1.V12/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BerezkinAA.Sprint3.Task1.V12.Lib;
+
+namespace Tyuiu.BerezkinAA.Sprint3.Task1.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Березкин А. А. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Березкин Артем Андреевич | ИСПб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму        *");
+            Console.WriteLine("* ряда по формуле, при n = 5                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine("Переменная n = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+        }
+    }
+}
