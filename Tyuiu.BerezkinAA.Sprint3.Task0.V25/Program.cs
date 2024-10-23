0aeb05b55bbbcef106d76e57cd21a2ee0933435c
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-10-23 23:53:50 +0500

    Task0

---
+++ Tyuiu.BerezkinAA.Sprint3.Task0.V25/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.BerezkinAA.Sprint3.Task0.V25.Lib;
+
+namespace Tyuiu.BerezkinAA.Sprint3.Task0.V25
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Березкин А. А. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Березкин Артем Андреевич | ИСПб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму          *");
+            Console.WriteLine("* ряда по формуле, при Х = 5                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 14;
+            Console.WriteLine("Переменная Х = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+        }
+    }
+}
+    
