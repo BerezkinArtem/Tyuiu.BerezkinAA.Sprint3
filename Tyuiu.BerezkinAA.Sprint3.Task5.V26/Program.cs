74259584b36717c16317fa56d1e92fe28b2d36da
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-10-24 19:45:08 +0500

    Task5

---
+++ Tyuiu.BerezkinAA.Sprint3.Task5.V26/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.BerezkinAA.Sprint3.Task5.V26.Lib;
+
+namespace Tyuiu.BerezkinAA.Sprint3.Task5.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #3 | Выполнил: Березкин А. А. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Березкин Артем Андреевич | ИСПб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет сумму   *");
+            Console.WriteLine("* ряда по формуле, при x = 0.7                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 13;
+
+            Console.WriteLine("Переменная x = " + x);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+        }
+    }
+}
